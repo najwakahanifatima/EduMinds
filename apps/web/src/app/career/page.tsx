056bdb92a7da6851669d937daaa66b7bab5da92d
naylzhra <naylaazahira9@gmail.com>
2025-07-04 21:27:44 +0700

    fix: size adjustments for career page

--- apps/web/src/app/career/page.tsx
+++ apps/web/src/app/career/page.tsx
@@ -2,14 +2,13 @@
 "use client";
 import React from "react";
 import Navbar from "@/app/_components/Navbar";
-import { useRouter, useParams } from "next/navigation";
 import JobCard from "@/app/_components/JobCard";
+import { useRouter } from "next/navigation";
 import { useRef } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
 export default function CareerPage() {
-  const router = useRouter(); 
-  /* dummy data */
+  const router = useRouter();
   const recommended = [
     { company: "PT Keren", title: "Pengrajin Bunga", location: "Bandung", time: "3 Bulan", salary: "Rp 5.000.000", path: "#" },
     { company: "PT Keren", title: "Penata Tanaman", location: "Bekasi", time: "6 Bulan", salary: "Rp 5.000.000", path: "#" },
@@ -30,6 +29,7 @@ export default function CareerPage() {
         icon: '/koki.png'
      },
   ];
+
   const listRef = useRef<HTMLDivElement>(null);
   const scrollByCard = (dir: "left" | "right") => {
     if (!listRef.current) return;
@@ -43,20 +43,20 @@ export default function CareerPage() {
     <>
       <Navbar />
 
-      <main className="mt-10 px-6 md:px-24 lg:px-40 xl:px-56 w-full overflow-hidden">
+      <main className="px:10 md:px-20 lg:px-40 w-full overflow-hidden">
         {/* HERO */}
         <section className="grid gap-8 md:grid-cols-2 md:items-center">
           <div>
-            <h1 className="text-4xl lg:text-5xl font-semibold leading-tight text-gray-900">
+            <h1 className="text-4xl font-semibold leading-tight text-gray-900">
               Cari Pekerjaan <br /> Impianmu!
             </h1>
 
             {/* search */}
-            <div className="relative mt-8 max-w-md">
+            <div className="relative mt-8 max-w-xs">
               <input
                 type="text"
                 placeholder="Cari pekerjaan..."
-                className="w-full bg-[#DCDDFF] rounded-xl border px-4 py-1.5 pr-9 text-base lg:text-lg outline-none focus:ring-2 focus:ring-black"
+                className="w-full bg-[#DCDDFF] rounded-xl border px-4 py-1.5 pr-9 text-sm outline-none focus:ring-2 focus:ring-black"
               />
               <img src="/search.png" alt="search icon"
                 className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5" />
@@ -69,14 +69,14 @@ export default function CareerPage() {
         </section>
 
         {/* REKOMENDASI UTAMA */}
-        <section className="mt-14">
+        <section className="mt-10">
           <div className="mb-6 flex items-baseline justify-between">
-            <h2 className="text-2xl font-semibold text-gray-900">
+            <h2 className="text-xl font-semibold text-gray-900">
               Rekomendasi pekerjaan berdasarkan jalur belajar
             </h2>
           </div>
 
-          <div className="grid gap-10 sm:grid-cols-2 md:grid-cols-3 justify-items-center justify-center md:mx-8 lg:mx-14">
+          <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 justify-items-center justify-center md:mx-5 lg:mx-10">
             {recommended.map((job) => (
               <JobCard key={job.title} {...job} />
             ))}
@@ -91,12 +91,12 @@ export default function CareerPage() {
         {/* CAROUSEL JOB LAIN */}
         <section className="mt-16">
           <div className="mb-6 flex items-baseline justify-between">
-            <h2 className="text-2xl font-semibold text-gray-900">
+            <h2 className="text-xl font-semibold text-gray-900">
               Lihat pekerjaan dari jalur belajar lain
             </h2>
             
           </div>
-          <div className="relative mb-5 md:mx-5 lg:mx-10" >
+          <div className="relative mb-5 mx-1 md:mx-3 lg:mx-8" >
             <button
               onClick={() => scrollByCard("left")}
               className="hidden md:flex absolute left-0 top-1/2 -translate-y-1/2 h-8 w-8 items-center justify-center rounded-full border bg-white shadow hover:bg-gray-100"
@@ -113,15 +113,15 @@ export default function CareerPage() {
                     key={c.name}
                     className="
                       flex min-w-[240px] flex-col items-center justify-between
-                      rounded-3xl p-6 text-center transition border bg-[#E9F7F0] mt-2"
+                      rounded-3xl p-6 text-center transition border bg-[#B3EBCE]/70"
                   >
-                    <div className="h-36 w-52 mb-2 flex items-center justify-center">
+                    <div className="h-16 w-16 lg:h-28 lg:w-28 mb-2 flex items-center justify-center">
                       <img src={c.icon} alt={c.name} className="max-h-full max-w-full" />
                     </div>
                     <h3 className="text-2xl text-[#1E1E1E]">{c.name}</h3>
                     <button 
                     onClick={() => router.push("/career/" + c.name.toLowerCase())}
-                    className="mt-6 mb-2 rounded-md border border-yellow-600 bg-yellow-400 px-3 py-1 text-xs font-bold hover:bg-yellow-500 hover:scale-105">
+                    className="mt-4 mb-2 rounded-md border border-[#1E1E1E] bg-[#FDE06C] px-3 py-1 text-xs font-semibold hover:scale-105">
                       Lihat Lowongan
                     </button>
                   </div>
